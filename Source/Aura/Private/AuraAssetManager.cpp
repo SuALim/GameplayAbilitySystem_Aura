2a78a6f94c73f9c017a8d5de90e408feee91c752
SuALim <tndk314@gmail.com>
2025-04-14 21:38:32 +0900

    8 Section Complete

---
+++ Source/Aura/Private/AuraAssetManager.cpp
@@ -0,0 +1,20 @@
+// Copyright 2025. NoBicDeal All rights reserced.
+
+
+#include "AuraAssetManager.h"
+#include "AuraGameplayTags.h"
+
+UAuraAssetManager& UAuraAssetManager::Get()
+{
+    check(GEngine);
+
+    UAuraAssetManager* AuraAssetManager = Cast<UAuraAssetManager>(GEngine->AssetManager);
+    return *AuraAssetManager;
+}
+
+void UAuraAssetManager::StartInitialLoading()
+{
+    Super::StartInitialLoading();
+
+    FAuraGameplayTags::InitializeNativeGameplayTags();
+}
---
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -0,0 +1,27 @@
+// Copyright 2025. NoBicDeal All rights reserced.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "AuraAbilitySystemLibrary.generated.h"
+
+class UAttributeMenuWidgetController;
+class UOverlayWidgetController;
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAbilitySystemLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+
+	UFUNCTION(BlueprintPure, Category = "AuraAbilitySystemLibrary|WidgetController")
+	static UOverlayWidgetController* GetOverlayWidgetController(const UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintPure, Category = "AuraAbilitySystemLibrary|WidgetController")
+	static UAttributeMenuWidgetController* GetAttributeMenuWidgetController(const UObject* WorldContextObject);
+};
